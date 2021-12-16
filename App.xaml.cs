0aa0abfe28a423151d82dfa1f6f6f8bd96d1778b
404proverka@gmail.com <404proverka@gmail.com>
2021-12-16 15:19:19 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace School2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities.user18test_importEntities Context
+        { get; } = new Entities.user18test_importEntities();
+    }
+}
